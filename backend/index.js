ffba619145225bcfb2f6afe4518637c34bea889b
wassimWM <tech.hub213@gmail.com>
2024-10-24 23:27:45 +0100

    add new changes

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,22 @@
-let x=10;
-console.log("hello i am wassim");
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    const offset = -currentSlide * 100; // Move the slider by percentage
+    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
